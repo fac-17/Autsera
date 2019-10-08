fa5838a06045197675dc4ac9b51d7e344ccf6e2f
Jan Skowronski <crianonim@gmail.com>
2019-10-08 13:03:29 +0100

    Add Stars component

--- src/components/PlacePage/PlacePage.js
+++ src/components/PlacePage/PlacePage.js
@@ -1,13 +1,16 @@
 import React from "react";
 import RouterLink from "../reusable/RouterLink";
 import data from "../../data/data";
+import Stars from "../reusable/Stars";
+
 const PlacePage = ({ id }) => {
   const placeData = data.places.find(place => place.id === Number(id));
-  console.log(placeData);
+  // console.log(placeData);
   return (
     <div style={{ backgroundImage: `url(/img/${placeData.image})` }}>
       <h2>{placeData.text}</h2>
       <RouterLink to="/map" label="Go Back" />
+      <Stars has="3" max="5" />
     </div>
   );
 };
