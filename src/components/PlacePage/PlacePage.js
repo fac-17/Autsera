fbfdb05867b3b33a23231d6bde37e32ade673b83
Victor Masson <victormasson21@gmail.com>
2019-10-08 16:00:54 +0100

    Stars (full and empty) display at the top of the place

--- src/components/PlacePage/PlacePage.js
+++ src/components/PlacePage/PlacePage.js
@@ -3,15 +3,18 @@ import RouterLink from "../reusable/RouterLink";
 import data from "../../data/data";
 import InteractionCircle from "./InteractionCircle";
 import Stars from "../reusable/Stars";
+import {countStarsInPlace} from "../../utils/starsCounting";
 
-const PlacePage = ({ id }) => {
+const PlacePage = ({ id, completed }) => {
   const placeData = data.places.find(place => place.id === Number(id));
+  const stars = countStarsInPlace(id, completed);
+
   return (
     <div style={{ backgroundImage: `url(/img/${placeData.image})` }}>
       <h2>{placeData.text}</h2>
       <RouterLink to="/map" label="Go Back" />
       {/* Sample Stars Component */}
-      <Stars has="3" max="5" />
+      <Stars has={stars.has} max={stars.max} />
       {placeData.interactions.map(interaction => (
         <InteractionCircle key={interaction.id} interaction={interaction} />
       ))}
