2332b408c251973d2d4f3784c9d4c5d485d3160d
Jan Skowronski <crianonim@gmail.com>
2019-10-08 13:57:12 +0100

    Using InteractionCircle in PlacePage. Relates #55

--- src/components/PlacePage/PlacePage.js
+++ src/components/PlacePage/PlacePage.js
@@ -1,6 +1,7 @@
 import React from "react";
 import RouterLink from "../reusable/RouterLink";
 import data from "../../data/data";
+import InteractionCircle from "./InteractionCircle"
 const PlacePage = ({ id }) => {
   const placeData = data.places.find(place => place.id === Number(id));
   // console.log(placeData.interactions);
@@ -8,7 +9,7 @@ const PlacePage = ({ id }) => {
     <div style={{ backgroundImage: `url(/img/${placeData.image})` }}>
       <h2>{placeData.text}</h2>
       <RouterLink to="/map" label="Go Back" />
-      {placeData.interactions.map(interaction=><RouterLink key={interaction.id} to={"/interaction/"+interaction.id} label={interaction.text}/>)}
+      {placeData.interactions.map(interaction=><InteractionCircle key={interaction.id} interaction={interaction} />)}
     </div>
   );
 };
