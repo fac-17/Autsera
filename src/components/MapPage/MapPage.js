436d7063cbc5de4115f186f90c6d1ece494fd33b
reubengt <reubengeorgethomas@gmail.com>
2019-10-08 14:42:30 +0100

    add PlaceCircle component and tests

--- src/components/MapPage/MapPage.js
+++ src/components/MapPage/MapPage.js
@@ -2,14 +2,16 @@ import React from "react";
 import RouterLink from "../reusable/RouterLink";
 import "./map.css";
 import data from "../../data/data";
+import PlaceCircle from "./PlaceCircle";
 
 const MapPage = props => {
-
   return (
     <div className="map-background">
       <h2>AUTSERA LAND</h2>
       <RouterLink to="/" label="Go Back" />
-      {data.places.map(place => <RouterLink key={place.id} to={`/place/${place.id}`} label={place.text} />)}
+      {data.places.map(place => (
+        <PlaceCircle key={place.id} place={place} />
+      ))}
     </div>
   );
 };
