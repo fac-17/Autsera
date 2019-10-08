be63cd74d3f5fa62d0331cd92a666f94b9f459b5
reubengt <reubengeorgethomas@gmail.com>
2019-10-08 17:37:09 +0100

    add class for locked or unlocked places

--- src/components/MapPage/PlaceCircle.js
+++ src/components/MapPage/PlaceCircle.js
@@ -2,11 +2,19 @@ import React from "react";
 import RouterLink from "../reusable/RouterLink";
 import Stars from "../reusable/Stars";
 import { countStarsInPlace } from "../../utils/starsCounting";
-const PlaceCircle = ({ place, completed }) => (
-  <div>
-    <RouterLink label={place.text} to={"/place/" + place.id}></RouterLink>
-    <Stars {...countStarsInPlace(place.id, completed)} />
-  </div>
-);
+import "./PlaceCircle.scss";
+const PlaceCircle = ({ place, completed }) => {
+  return (
+    <div
+      className={
+        "place-circle " +
+        (completed.length >= place.requiredStars ? "unlocked" : "locked")
+      }
+    >
+      <RouterLink label={place.text} to={"/place/" + place.id}></RouterLink>
+      <Stars {...countStarsInPlace(place.id, completed)} />
+    </div>
+  );
+};
 
 export default PlaceCircle;
