58c1eef7b079febc442609284cf7a747e97af1b7
reubengt <reubengeorgethomas@gmail.com>
2019-10-09 12:54:56 +0100

    add failing test for both speechText and helpText passed in to helperavatar

--- src/components/reusable/HelperAvatar.test.js
+++ src/components/reusable/HelperAvatar.test.js
@@ -5,7 +5,7 @@ const helpText = "Look again, are they smiling?";
 const speechText = "Hi i'm a hippo";
 test("The helper component renders with the right text", () => {
   const { getByText } = render(<HelperAvatar speechText={speechText} />);
-  getByText("Hi i'm a hippo");
+  getByText(speechText);
 });
 test("The image of the helper appears when there is no speech provided", () => {
   const { getByAltText } = render(<HelperAvatar />);
@@ -17,5 +17,18 @@ test("The helper component shows the helpText when clicked", async () => {
   );
   const avatar = getByAltText("helper-avatar");
   fireEvent.click(avatar);
-  getByText("Look again, are they smiling?");
+  getByText(helpText);
+});
+test("When both speechText and helpText are passed in , the helper component replaces the speechText with the helpText when clicked", () => {
+  const { getByAltText, getByText } = render(
+    <HelperAvatar helpText={helpText} speechText={speechText} />
+  );
+  const avatar = getByAltText("helper-avatar");
+  getByText(speechText);
+  //expect speechText to be displayed and helpText to not be displayed on initial render;
+  expect(getByText(helpText)).toThrow();
+  fireEvent.click(avatar);
+  //expect speechText to disappear and helpText to be displayed after clicking
+  expect(getByText(speechText)).toThrow();
+  getByText(helpText);
 });
