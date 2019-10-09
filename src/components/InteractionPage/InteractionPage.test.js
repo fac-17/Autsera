8376b8f59f854c721bff60ee722ecd2d346b255f
Leonie Zorzi <Leonie.Zorzi@hotmail.com>
2019-10-09 12:38:21 +0100

    Adds interaction test to test if correct class is added Relates #26

--- src/components/InteractionPage/InteractionPage.test.js
+++ src/components/InteractionPage/InteractionPage.test.js
@@ -25,3 +25,19 @@ test("Game starts from scratch when user re-enters page", () => {
     expect(getByText(answer.text).className).toBe("option")
   );
 });
+
+test("Selecting an answer changes the class to 'selected'", () => {
+  const { getByText, getByLabelText, getByTestId } = render(
+    <Router>
+      <InteractionPage id="1" />
+    </Router>
+  );
+  const answers = data.places[0].interactions[0].answers;
+  const oneAnswer = getByText(answers[0].text);
+  fireEvent.click(oneAnswer);
+  if (answers[0].correct) {
+    expect(oneAnswer.className).toBe("selected");
+  } else {
+    expect(oneAnswer.className).toBe("option");
+  }
+});
