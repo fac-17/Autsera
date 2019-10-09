286b33a8eb97fd3542ed9e1152377003cb5e8553
Leonie Zorzi <Leonie.Zorzi@hotmail.com>
2019-10-09 12:19:55 +0100

    Adds interaction test to test if all answers have class of option when game starts Relates #29

--- src/components/InteractionPage/InteractionPage.test.js
+++ src/components/InteractionPage/InteractionPage.test.js
@@ -1,12 +1,27 @@
 import React from "react";
-import { render, fireEvent } from "@testing-library/react";
+import { render, fireEvent, cleanup } from "@testing-library/react";
 import InteractionPage from "./InteractionPage";
 import { BrowserRouter as Router } from "react-router-dom";
+import data from "../../data/data";
+
+afterEach(cleanup);
 
 test("The Interaction Page renders", () => {
   render(
     <Router>
       <InteractionPage id="1" />
     </Router>
+  );
+});
+
+test("Game starts from scratch when user re-enters page", () => {
+  const { getByText, getByLabelText, getByTestId } = render(
+    <Router>
+      <InteractionPage id="1" />
+    </Router>
+  );
+  const answers = data.places[0].interactions[0].answers;
+  answers.map(answer =>
+    expect(getByText(answer.text).className).toBe("option")
   );
 });
