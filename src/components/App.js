c6e04e61b4412e6c3e3bd4c543c702311e15553e
reubengt <reubengeorgethomas@gmail.com>
2019-10-10 16:09:12 +0100

    use router Switch to redirect to 404 page if no endpoint is matched

--- src/components/App.js
+++ src/components/App.js
@@ -1,49 +1,54 @@
 import React, { useState, useEffect } from "react";
 import "./App.scss";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import HomePage from "./HomePage/HomePage";
 import HelpPage from "./HelpPage/HelpPage";
 import MapPage from "./MapPage/MapPage";
 import PlacePage from "./PlacePage/PlacePage";
 import InteractionPage from "./InteractionPage/InteractionPage";
-
+import FourOFour from "./FourOFour/FourOFour";
 function App() {
-  const [completed, setCompleted] = useState(JSON.parse(localStorage.getItem("completed")) || []);
+  const [completed, setCompleted] = useState(
+    JSON.parse(localStorage.getItem("completed")) || []
+  );
 
   useEffect(() => {
     localStorage.setItem("completed", JSON.stringify(completed));
-  }, [completed])
+  }, [completed]);
 
   return (
     <div className="App">
       <Router>
-        <Route exact path="/" component={HomePage} />
-        <Route path="/help" component={HelpPage} />
-        <Route
-          path="/map"
-          render={() => (
-            <MapPage completed={completed} setCompleted={setCompleted} />
-          )}
-        />
-        <Route
-          path="/place/:id"
-          render={({ match }) => (
-            <PlacePage
-              id={Number(match.params.id)}
-              completed={completed}
-              setCompleted={setCompleted}
-            />
-          )}
-        />
-        <Route
-          path="/interaction/:id"
-          render={({ match }) => (
-            <InteractionPage
-              id={Number(match.params.id)}
-              setCompleted={setCompleted}
-            />
-          )}
-        />
+        <Switch>
+          <Route exact path="/" component={HomePage} />
+          <Route path="/help" component={HelpPage} />
+          <Route
+            path="/map"
+            render={() => (
+              <MapPage completed={completed} setCompleted={setCompleted} />
+            )}
+          />
+          <Route
+            path="/place/:id"
+            render={({ match }) => (
+              <PlacePage
+                id={Number(match.params.id)}
+                completed={completed}
+                setCompleted={setCompleted}
+              />
+            )}
+          />
+          <Route
+            path="/interaction/:id"
+            render={({ match }) => (
+              <InteractionPage
+                id={Number(match.params.id)}
+                setCompleted={setCompleted}
+              />
+            )}
+          />
+          <Route path="*" component={FourOFour} />
+        </Switch>
       </Router>
     </div>
   );
