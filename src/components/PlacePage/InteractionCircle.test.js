5acd1015c52c02b6d1e8a748c1349f0d808e1eb1
reubengt <reubengeorgethomas@gmail.com>
2019-10-15 13:04:58 +0100

    fix interactioncircle link failing test'

--- src/components/PlacePage/InteractionCircle.test.js
+++ src/components/PlacePage/InteractionCircle.test.js
@@ -16,39 +16,39 @@ const example = {
       id: 2,
       text: "they are fighting",
       correct: true,
-      response: "Correct! They look like they are fighting"
+      response: "Correct! They look like they are fighting",
     },
     {
       id: 3,
       text: "playing",
       correct: false,
-      response: "Look again! Do they look like they are having fun?"
+      response: "Look again! Do they look like they are having fun?",
     },
     {
       id: 4,
       text: "upset",
       correct: true,
-      response: "Correct! They look upset"
+      response: "Correct! They look upset",
     },
     {
       id: 5,
       text: "happy",
       correct: false,
-      response: "Look again, are they smiling?"
-    }
+      response: "Look again, are they smiling?",
+    },
   ],
   hints: [
     {
       id: 6,
       text: "Look at their faces, are they smiling?",
-      answers: [4, 5]
+      answers: [4, 5],
     },
     {
       id: 7,
       text: "Look if they are having fun?",
-      answers: [2, 3]
-    }
-  ]
+      answers: [2, 3],
+    },
+  ],
 };
 
 test("if it renders the InteractionCircle", () => {
@@ -71,7 +71,7 @@ test("if it has the name of the interaction", () => {
 test("if it has the link of the interaction", () => {
   const { getByTitle } = render(
     <Router>
-      <InteractionCircle interaction={example} />
+      <InteractionCircle interaction={example} isUnlocked={true} />
     </Router>
   );
   const link = getByTitle(example.text);
