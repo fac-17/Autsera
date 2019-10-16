c4f2e5a1d5033743477f2018331211eb37a349a3
reubengt <reubengeorgethomas@gmail.com>
2019-10-16 13:35:27 +0100

    add responsiveness fixes for homepage and map page

--- src/components/HomePage/HomePage.js
+++ src/components/HomePage/HomePage.js
@@ -4,29 +4,31 @@ import RouterLink from "../reusable/RouterLink";
 import HelperAvatar from "../reusable/HelperAvatar";
 import "./homepage.scss";
 
-const HomePage = ({ history, setCompleted ,completed}) => {
+const HomePage = ({ history, setCompleted, completed }) => {
   return (
-    <div className="background-home">
-      {/* <RouterLink className="btn-help" to="/help" label="?" /> */}
-      <div className="container-home">
-        <img className="img-logo" src="./img/Autsera.png" alt="Autsera Logo" />
-        {completed.length?<RouterLink className="btn-start" to="/map" label="Continue Playing" />:null}
-        
-        <button  className="btn-newstart" onClick={() => {
-          setCompleted([]);
-          history.push("/map");
-        }} >New Game</button>
+    <>
+      <div className="background-home">
+        {/* <RouterLink className="btn-help" to="/help" label="?" /> */}
+        <div className="container-home">
+          <img className="img-logo" src="./img/Autsera.png" alt="Autsera Logo" />
+          {completed.length ? <RouterLink className="btn-start" to="/map" label="Continue Playing" /> : null}
+
+          <button
+            className="btn-newstart"
+            onClick={() => {
+              setCompleted([]);
+              history.push("/map");
+            }}
+          >
+            New Game
+          </button>
+        </div>
       </div>
       <HelperAvatar
-        speechText={
-          <RouterLink
-            to="/help"
-            label="Hi, welcome to Autsera. Click here if you need help"
-          />
-        }
+        speechText={<RouterLink to="/help" label="Hi, welcome to Autsera. Click here if you need help" />}
         timeOut={3600000}
       />
-    </div>
+    </>
   );
 };
 
