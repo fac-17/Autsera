a801cb1d0351487e70b640365fa6edf61c00af1a
Victor Masson <victormasson21@gmail.com>
2019-10-17 13:17:26 +0100

    Add play button with useRef()

--- src/components/HelpPage/HelpPage.js
+++ src/components/HelpPage/HelpPage.js
@@ -1,20 +1,42 @@
-import React from "react";
+import React, { useState, useEffect, useRef } from "react";
 import RouterLink from "../reusable/RouterLink";
 import "./helppage.scss";
 import HelperAvatar from "../reusable/HelperAvatar";
 
 const HelpPage = props => {
+  const [play, setPlay] = useState(true);
+  const videoEl = useRef();
+
+  const togglePlay = () => {
+    if (play) {
+      videoEl.current.pause();
+      videoEl.current.innerHtml = "Play";
+      setPlay(false);
+    } else {
+      videoEl.current.play();
+      videoEl.current.innerHtml = "Pause";
+      setPlay(true);
+    }
+  };
+
   return (
     <div className="background-help">
       <br />
       <RouterLink className="btn-back" to="/" label="Go Back" />
       <div className="video-container">
-        <video className="video-player" controls controlsList="nofullscreen nodownload" muted loop autoplay >
+        <video ref={videoEl} className="video-player" autoPlay >
           <source src="/AutseraHelpFinal.mp4" type="video/mp4" />
         </video>
+        <button
+          onClick={togglePlay}
+          className="play-button" >
+          {play ? "Pause" : "Play"}
+        </button>
       </div>
     </div>
   );
 };
 
 export default HelpPage;
+
+// <video ref={videoEl} className="video-player" controls controlsList="nofullscreen nodownload" muted loop autoplay >
